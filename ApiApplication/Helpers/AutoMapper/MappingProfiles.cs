7c7d5ff8d2a6c6f735733ae19232e7eb6f92028a
cenk <cenkciplak@windowslive.com>
2022-02-28 23:56:22 +0300

    Added automapper profiles.

---
+++ ApiApplication/Helpers/AutoMapper/MappingProfiles.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Cinema.Entities.Concrete;
+using Cinema.Entities.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiApplication.Helpers.AutoMapper
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<MovieDto, MovieEntity>().ReverseMap();                  
+            CreateMap<ShowtimeEntity, ShowtimeDto>().ReverseMap();
+            CreateMap<ImdbMovieDto, MovieEntity>().ForMember(dest => dest.ImdbId, opt => opt.MapFrom(src => src.Id)).ForMember(x => x.Id, y => y.Ignore());
+        }       
+    }
+}
