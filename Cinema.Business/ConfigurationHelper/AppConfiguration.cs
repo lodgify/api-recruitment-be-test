bab95e998a6a7cd65276ba0d6d399c378e230317
cenk <cenkciplak@windowslive.com>
2022-02-28 23:30:33 +0300

    Implemented configuration helper  to get imdb service information.

---
+++ Cinema.Business/ConfigurationHelper/AppConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Cinema.Business.ConfigurationHelper
+{
+    public class AppConfiguration : IAppConfiguration
+    {
+        public readonly string _baseUrl = string.Empty;
+        public readonly string _key = string.Empty;
+        public AppConfiguration()
+        {
+            var configurationBuilder = new ConfigurationBuilder();
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "appsettings.json");
+            configurationBuilder.AddJsonFile(path, false);
+
+            var root = configurationBuilder.Build();
+            _baseUrl = root.GetSection("ImdbApi").GetSection("BaseUrl").Value;
+            _key = root.GetSection("ImdbApi").GetSection("Key").Value;
+
+        }
+        public string BaseUrl
+        {
+            get => _baseUrl;
+        }
+
+        public string Key
+        {
+            get => _key;
+        }
+
+    }
+}
---
+++ Cinema.Business/ConfigurationHelper/IAppConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cinema.Business.ConfigurationHelper
+{
+    public interface IAppConfiguration
+    {
+        string BaseUrl { get; }
+        string Key { get; }
+    }
+}
