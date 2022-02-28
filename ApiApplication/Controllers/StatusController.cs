3ebd549f4042e45837431080e4534cc6984c64eb
cenk <cenkciplak@windowslive.com>
2022-02-28 23:58:24 +0300

    Controller and hosted service implementations.

---
+++ ApiApplication/Controllers/StatusController.cs
@@ -0,0 +1,35 @@
+﻿using Cinema.Business.Abstract;
+using Cinema.Entities.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiApplication.Controllers
+{
+
+    public class StatusController : BaseApiController
+    {
+        IStatusService _statusService;
+
+        public StatusController(IStatusService statusService)
+        {
+            _statusService = statusService;
+        }
+
+        [HttpGet]        
+        public IActionResult GetStatus()
+        {
+            // Getting the current status of Imdb api 
+            var response = _statusService.GetStatus();
+            if (response.Success)
+            {
+                return Ok(response);
+            }
+            return BadRequest();
+            
+        }
+    }
+}
