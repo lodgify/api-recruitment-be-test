19bec6b0bb758667106b34507cf947d539bb53c9
cenk <cenkciplak@windowslive.com>
2022-02-28 23:31:08 +0300

    Added constant messages class to escape from magic string.

---
+++ Cinema.Business/Constants/Messages.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cinema.Business.Constants
+{
+    public static class Messages
+    {
+        // Showtime
+        public static string ShowtimeAdded = "Showtime Added!";
+        public static string ShowtimeDeleted = "Showtime Deleted!";
+        public static string ShowtimeUpdated = "Showtime Updated!";
+        public static string EntityNotFound = "Entity not found!";
+        public static string ShowtimeNotFound = "No Showtime found!";
+        public static string ShotimeListSuccess = "Showtime List Success!";
+        public static string ShowtimeOverlap = "Showtime Dates Overlaps For The Specified Auditorium. Please Check Your Date Ranges.";
+        public static string AuditoriumNotFound = "Auditorium not found!";
+        public static string StatusUpdated = "Imdb Status Updated";
+        public static string DateError = "Check Dates";
+
+
+    }
+}
