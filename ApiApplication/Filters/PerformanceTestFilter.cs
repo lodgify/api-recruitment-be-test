85882ce41478dbfd2b99acd2b535b7415c0d4343
Ali Saeedi <ali0saeedi@gmail.com>
2022-03-01 12:55:51 +0300

    bugfix - performance log

--- ApiApplication/Filters/PerformanceTestFilter.cs
+++ ApiApplication/Filters/PerformanceTestFilter.cs
@@ -1,13 +1,14 @@
 ﻿using Microsoft.AspNetCore.Mvc.Filters;
 using System.Diagnostics;
 using NLog;
+using System;
 
 namespace ApiApplication.Filters
 {
     public class PerformanceTestFilter : IActionFilter
     {
         private Stopwatch stopWatch = new Stopwatch();
-        private static Logger logger = LogManager.GetCurrentClassLogger();
+        Logger logger = LogManager.GetCurrentClassLogger();
         public void OnActionExecuting(ActionExecutingContext filterContext)
         {
             stopWatch.Reset();
@@ -18,7 +19,7 @@ namespace ApiApplication.Filters
         {
             stopWatch.Stop();
             var executionTime = stopWatch.ElapsedMilliseconds;
-            logger.Debug($"executionTime for : {filterContext.RouteData.Values["controller"]} / {filterContext.RouteData.Values["action"]} is : {executionTime} ms.");            
+            logger.Debug($" ExecutionTime for : {filterContext.RouteData.Values["controller"]} / {filterContext.RouteData.Values["action"]} is : {executionTime} ms.");
         }
     }
 }
