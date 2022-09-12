3fbcf88374453ebb0f087b5f504aabce3afd5230
Enrique <enrique@ortuno.net>
2022-09-11 21:16:09 -0400

    unit test example implemented

---
+++ ApiApplication.Tests/Shared/MovieMother.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using ApiApplication.Resources;
+
+namespace ApiApplication.Tests.Shared
+{
+    public sealed class MovieMother
+    {
+        public static Movie Create(string imdbId, string Title)
+        {
+            return new Movie()
+            {
+                ImdbId = imdbId,
+                Stars = "Enrique",
+                ReleaseDate = DateTime.Now.AddYears(-4).ToString(),
+                Title = Title
+            };
+        }
+    }
+}
+
