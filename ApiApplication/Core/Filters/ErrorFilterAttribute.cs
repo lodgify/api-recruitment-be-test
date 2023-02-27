c2316ce6c41bb39c55641752973a4c9184761c2c
Diogo de Freitas Nunes <diogon1@hotmail.com>
2023-02-27 10:56:41 -0300

    [ApiApplication] ErrorFilterAttribute Created

---
+++ ApiApplication/Core/Filters/ErrorFilterAttribute.cs
@@ -0,0 +1,44 @@
+﻿using ApiApplication.Core.Notification;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Logging;
+
+namespace ApiApplication.Core.Filters
+{
+
+    public class ErrorFilterAttribute : ActionFilterAttribute, IActionFilter
+    {
+        #region [prop]
+
+        private readonly ILogger _logger;
+        private readonly IDomainNotification _domainNotification;
+
+        #endregion [prop]
+
+        #region [ctor]
+
+        public ErrorFilterAttribute(ILogger logger, IDomainNotification domainNotification)
+        {
+            _logger = logger;
+            _domainNotification = domainNotification;
+        }
+
+        #endregion [ctor]
+
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            if (filterContext.Exception != null)
+            {
+                _logger.LogError(filterContext.Exception.ToString());
+            }
+
+            if (_domainNotification.HasNotification)
+            {
+                var message = string.Join(',', _domainNotification.GetNotifications.Select(s => s));
+                var result = new BadRequestObjectResult($"{message}");
+                filterContext.Result = result;
+            }
+        }
+    }
+
+}
