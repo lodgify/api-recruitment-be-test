65b53a31ec14428c9570cc0e41d5c942d62838e0
Diogo de Freitas Nunes <diogon1@hotmail.com>
2023-02-27 10:50:58 -0300

    [ApiApplication] DomainNotification Added

---
+++ ApiApplication/Core/Notification/IDomainNotification.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace ApiApplication.Core.Notification
+{
+    public interface IDomainNotification
+    {
+        bool HasNotification { get; }
+
+        IEnumerable<string> GetNotifications { get; }
+        void AddRange(IEnumerable<string> notifications);
+        void Add(string notification);
+        void SetUniqueNotification(string notification);
+        void Clear();
+    }
+}
