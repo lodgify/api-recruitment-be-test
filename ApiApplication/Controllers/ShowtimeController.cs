ea6aa26d20fb2ba7d12d3883d38e19f58a848fdd
Diogo de Freitas Nunes <diogon1@hotmail.com>
2023-02-27 14:24:03 -0300

    Revert "[ApiApplication]"
    This reverts commit bdb196c16374bed4713fdd16abf6aa3bad988076.

--- ApiApplication/Controllers/ShowtimeController.cs
+++ ApiApplication/Controllers/ShowtimeController.cs
@@ -1,10 +1,8 @@
 ﻿using ApiApplication.Core.Base;
-using ApiApplication.Core.Filters;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ApiApplication.Controllers
 {
-    [ServiceFilter(typeof(MetricsFilterAttribute))]
     public class ShowtimeController : BaseController
     {
         public IActionResult Index()
