edcef37c90e61c06f7c004bfe1119398e052a4de
Diogo de Freitas Nunes <diogon1@hotmail.com>
2023-02-27 17:23:12 -0300

    [Application] - ShowtimeController post implementation and Executeasync to abstract execution in baseController

--- ApiApplication/Controllers/ShowtimeController.cs
+++ ApiApplication/Controllers/ShowtimeController.cs
@@ -1,13 +1,40 @@
-﻿using ApiApplication.Core.Base;
+﻿using ApiApplication.Application.Command;
+using ApiApplication.Core.Base;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace ApiApplication.Controllers
 {
+    [Microsoft.AspNetCore.Mvc.Route("api/[controller]")]
+    [ApiController]
     public class ShowtimeController : BaseController
     {
-        public IActionResult Index()
+        private IAddShowTimeCommandHandler _addShowTimeCommandHandler;
+
+        public ShowtimeController(IAddShowTimeCommandHandler addShowTimeCommandHandler)
         {
-            return Ok();
+            _addShowTimeCommandHandler = addShowTimeCommandHandler;
+        }
+
+        [AllowAnonymous]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [Microsoft.AspNetCore.Mvc.HttpPost("")]
+        public async Task<IActionResult> Post(AddShowTimeRequest command, CancellationToken cancellationToken)
+        {
+            return await ExecuteAsync(async () => await _addShowTimeCommandHandler.ExecuteAsync(command, cancellationToken));
+        }
+
+        [AllowAnonymous]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [Microsoft.AspNetCore.Mvc.HttpPatch("/api/TestamentoDigital/{guid}/AlterarTipo")]
+        public async Task<IActionResult> Patch(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
         }
     }
 }
--- ApiApplication/Core/Base/BaseController.cs
+++ ApiApplication/Core/Base/BaseController.cs
@@ -1,8 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ApiApplication.Core.CQRS;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
 
 namespace ApiApplication.Core.Base
 {
     public abstract class BaseController : ControllerBase
     {
+        protected async Task<IActionResult> ExecuteAsync(Func<Task<IResponse>> executeFunction, IActionResult resultForNull = null)
+        {
+            var response = await executeFunction();
+
+            return response == null
+             ? resultForNull ?? BadRequest()
+             : Ok(response);
+        }
     }
 }
