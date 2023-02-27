0713a503c57d48c71164794c40c16ad0c501b4f6
Diogo de Freitas Nunes <diogon1@hotmail.com>
2023-02-27 09:58:21 -0300

    [ApiApplication]  - Created ShowTimeController
    [Lodgify.Cinema.UnitTest]
    - Created a layer to do the unit tests

---
+++ ApiApplication/Controllers/ShowtimeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiApplication.Controllers
+{
+    public class ShowtimeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
